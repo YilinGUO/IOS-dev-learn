b729a4b87af1a7fa7cb19e0976675b7eec3736c3
Elaine Guo <YilinGUO>
2016-08-13 11:15:04 -0400

    Initial commit

---
+++ hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello
+//
+//  Created by Elaine Guo on 8/12/16.
+//  Copyright © 2016 Elaine Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello
+//
+//  Created by Elaine Guo on 8/12/16.
+//  Copyright © 2016 Elaine Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
